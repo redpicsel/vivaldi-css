b3f4b0c430d0057af99dbe019a930de798bae89c
Lực Nguyễn <iamnguyentanluc@gmail.com>
2023-12-26 11:18:53 +0700

    update css

--- custom.js
+++ custom.js
@@ -1,1 +1,8 @@
-﻿document.getElementsByClassName("vivaldi")[0].title = "Lực Nguyễn";
+﻿if (document.getElementsByClassName("vivaldi") && document.getElementsByClassName("vivaldi").length > 0)
+	document.getElementsByClassName("vivaldi")[0].title = "Lực Nguyễn";
+
+
+
+
+//body tag in window.html
+//<script src="custom.js"></script>
