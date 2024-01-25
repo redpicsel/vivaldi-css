a856bbe3707a7aa92426a59308fd72f1b1c39bc6
Lực Nguyễn <iamnguyentanluc@gmail.com>
2024-01-25 15:02:47 +0700

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,115 +1,144 @@
 ﻿//window.onload = function (event) {
 setTimeout(function () {
-	if (document.getElementsByClassName("vivaldi") && document.getElementsByClassName("vivaldi").length > 0)
-		document.getElementsByClassName("vivaldi")[0].title = "Lực Nguyễn";
+    if (document.getElementsByClassName("vivaldi") && document.getElementsByClassName("vivaldi").length > 0)
+        document.getElementsByClassName("vivaldi")[0].title = "Lực Nguyễn";
 
     clearSearchInput();
 
     openLinkDialog();
 
 
-	if (document.getElementById("urlFieldInput")) {
-		document.getElementById("urlFieldInput").onchange = function () {
-			setTimeout(function () {
-				var currentUrl = document.getElementById("urlFieldInput").value;
+    // if (document.getElementById("urlFieldInput")) {
+    // 	document.getElementById("urlFieldInput").onchange = function () {
+    // 		setTimeout(function () {
+    // 			var currentUrl = document.getElementById("urlFieldInput").value;
 
-				if (currentUrl.includes("StartPageChrome2")) {
-					clearSearchInput();
+    // 			if (currentUrl.includes("StartPageChrome2")) {
+    // 				clearSearchInput();
 
-					return;
-				}
-
-
-				if (currentUrl.includes("localhost") || currentUrl.includes("127.0.0.1")) {
-					setTimeout(function () {
-						localhost();
-					}, 200);
-
-					return;
-				}
+    // 				return;
+    // 			}
 
 
-				setTimeout(function () {
-					showPort();
-				}, 200);
+    // 			if (currentUrl.includes("localhost") || currentUrl.includes("127.0.0.1")) {
+    // 				setTimeout(function () {
+    // 					localhost();
+    // 				}, 200);
+
+    // 				return;
+    // 			}
 
 
-				//var port = getPort(currentUrl);
-				//if (port != "") {
-				//	setTimeout(function () {
-				//		showPort();
-				//	}, 200);
-
-				//	return;
-				//}
+    // 			setTimeout(function () {
+    // 				showPort();
+    // 			}, 200);
 
 
-			}, 500)
-		};
-	}
+    // 			//var port = getPort(currentUrl);
+    // 			//if (port != "") {
+    // 			//	setTimeout(function () {
+    // 			//		showPort();
+    // 			//	}, 200);
+
+    // 			//	return;
+    // 			//}
+
+
+    // 		}, 500)
+    // 	};
+    // }
+
+
+
+    if (document.getElementById("urlFieldInput")) {
+        setInterval(function () {
+            var currentUrl = document.getElementById("urlFieldInput").value;
+
+            if (currentUrl.includes("StartPageChrome2")) {
+                clearSearchInput();
+
+                return;
+            }
+
+            if (currentUrl.includes("localhost") || currentUrl.includes("127.0.0.1")) {
+                setTimeout(function () {
+                    localhost();
+                }, 200);
+
+                return;
+            }
+
+            setTimeout(function () {
+                showPort();
+            }, 200);
+        }, 2000)
+    }
+
+
+
 }, 2000)
 //};
 
 showPort = function () {
-	var currentUrl = document.getElementById("urlFieldInput").value;
-	var port = getPort(currentUrl);
+    var currentUrl = document.getElementById("urlFieldInput").value;
+    var port = getPort(currentUrl);
 
-	if (document.querySelectorAll("#tabs-container .tab.active").length > 0) {
-		var tabActive = document.querySelectorAll("#tabs-container .tab.active")[0];
-		var title = tabActive.title;
+    if (document.querySelectorAll("#tabs-container .tab.active").length > 0) {
+        var tabActive = document.querySelectorAll("#tabs-container .tab.active")[0];
+        var title = tabActive.title;
 
-		var portHTML = port == "" ? "" : port + "<span class='port-title'>-</span>";
+        var portHTML = port == "" ? "" : port + "<span class='port-title'>-</span>";
 
-		//tabActive.title = port + " ● " + title;
-		tabActive.getElementsByClassName("title")[0].innerHTML = portHTML + title;
-	}
+        //tabActive.title = port + " ● " + title;
+        tabActive.getElementsByClassName("title")[0].innerHTML = portHTML + title;
+    }
 }
 
 localhost = function () {
-	if (document.querySelectorAll("#tabs-container .tab.active").length > 0) {
-		var tabActive = document.querySelectorAll("#tabs-container .tab.active")[0];
-		var parent = tabActive.parentElement;
-		parent.classList.add("localhost");
+    if (document.querySelectorAll("#tabs-container .tab.active").length > 0) {
+        var tabActive = document.querySelectorAll("#tabs-container .tab.active")[0];
+        var parent = tabActive.parentElement;
+        parent.classList.add("localhost");
 
-		var title = tabActive.title;
+        var title = tabActive.title;
 
-		//tabActive.title = "Local ● " + title;
-		tabActive.getElementsByClassName("title")[0].innerHTML = "local" + "<span class='port-title'>-</span>" + title;
-	}
+        //tabActive.title = "Local ● " + title;
+        tabActive.getElementsByClassName("title")[0].innerHTML = "local" + "<span class='port-title'>-</span>" + title;
+    }
 }
 
 function getPort(url) {
-	url = url.match(/^(([a-z]+:)?(\/\/)?[^\/]+).*$/)[1] || url;
+    url = url.match(/^(([a-z]+:)?(\/\/)?[^\/]+).*$/)[1] || url;
 
-	var parts = url.split(':'),
-		port = parseInt(parts[parts.length - 1], 10);
+    var parts = url.split(':'),
+        port = parseInt(parts[parts.length - 1], 10);
 
-	if (parts[0] === 'http' && (isNaN(port) || parts.length < 3))
-		return "";
+    if (parts[0] === 'http' && (isNaN(port) || parts.length < 3))
+        return "";
 
-	if (parts[0] === 'https' && (isNaN(port) || parts.length < 3))
-		return "";
+    if (parts[0] === 'https' && (isNaN(port) || parts.length < 3))
+        return "";
 
-	if (parts.length === 1 || isNaN(port))
-		return "";
+    if (parts.length === 1 || isNaN(port))
+        return "";
 
-	return port;
+    return port;
 }
 
 clearSearchInput = function () {
-	if (document.getElementById("urlFieldInput")) {
-		document.getElementById("urlFieldInput").value = "";
+    if (document.getElementById("urlFieldInput")) {
+        document.getElementById("urlFieldInput").value = "";
 
-		setTimeout(function () {
-			//document.getElementById("urlFieldInput").focus();
-		}, 100);
-	}
+        setTimeout(function () {
+            //document.getElementById("urlFieldInput").focus();
+        }, 100);
+    }
 }
 
 
 
 //-----------open link in dialog------------------------------------------------------------------------------------------------------
-function openLinkDialog () {
+function openLinkDialog() {
     let searchEngineCollection,
         defaultSearchId,
         privateSearchId,
